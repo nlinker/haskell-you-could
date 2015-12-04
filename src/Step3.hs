4cc8f9549a6266b3ee654ca79f1a8eea869a4273
Nick Linker <nlinker@issart.com>
2015-12-04 18:02:09 +0600

    added something

--- src/Step3.hs
+++ src/Step3.hs
@@ -1,3 +1,33 @@
 module Step3 where
 
--- Monads
+-- # Лямбдочки
+-- Синоним для термина "анонимная функция"
+
+test1 :: IO ()
+test1 = do
+  let xs = [1, 2, 3]
+  let ys = map double xs
+  print ys
+  where
+    double x = 2 * x
+
+test2 :: IO ()
+test2 = do
+  let xs = [1, 2, 3]
+  let ys = map double xs
+  print ys
+  where
+    double = \x -> 2 * x  -- <- Различие здесь
+
+------------------------------
+-- Какие типы у этих функций?
+
+sum4 = \xs -> sum xs
+
+add_42 = \x -> x + 42
+
+-- Как правильно расставить скобки?
+omg_add = \x -> \y -> x + y
+
+sum3 = \x y z -> x + y + z
+
